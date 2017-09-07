067db858533c95c9efd8edf306845e6650fd063e
teamDBSD <kaddm@yandex.ru>
2017-09-07 12:41:19 +0300

    server classes added send command handling

--- src/main/java/com/team3chat/server/Server.java
+++ src/main/java/com/team3chat/server/Server.java
@@ -1,6 +1,5 @@
 package com.team3chat.server;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -8,8 +7,8 @@ import java.net.ServerSocket;
 public class Server {
     public static void main(String[] args) throws IOException {
         HistoryDealer historyDealer = new HistoryDealer(new File("history.txt"));
-        ServerSocket serverSocket = new ServerSocket(1234);
         BusinessLogic logicApplier = new BusinessLogic(historyDealer);
+        ServerSocket serverSocket = new ServerSocket(1234);
 
         while (true) {
             new Thread(new Acceptor(serverSocket.accept(), logicApplier)).start();
