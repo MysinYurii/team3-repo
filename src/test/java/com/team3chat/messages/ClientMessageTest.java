b422295815d481778d436a5822f16957f00c2af8
zlv <zlvlezhnin@gmail.com>
2017-09-07 12:32:39 +0300

    Added test fails

--- src/test/java/com/team3chat/messages/ClientMessageTest.java
+++ src/test/java/com/team3chat/messages/ClientMessageTest.java
@@ -2,22 +2,45 @@ package com.team3chat.messages;
 
 import com.team3chat.client.ChatClient;
 import com.team3chat.client.ChatClientRealisation;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
 
 import static org.mockito.Mockito.times;
+import static org.mockito.MockitoAnnotations.initMocks;
 
-@Ignore
 public class ClientMessageTest {
+    private ChatClientRealisation client;
 
-    @Test
-    public void shouldGetMessageWithDateAndTimeWhenSendMessage() {
+    @Before
+    public void setUp() {
         //Given
         String serverAddress = "127.0.0.1";
         int port = 1234;
-        ChatClient client = new ChatClientRealisation(serverAddress, port);
+        client = new ChatClientRealisation(serverAddress, port);
+        initMocks(this);
+    }
+
+    @Ignore
+    @Test
+    public void shouldGetMessageWithDateAndTimeWhenSendMessage() {
+        //When
         client.message("/snd message test");
-        verify(client, times(1)).lastMessage("time message test");
+
+        //Then
+        verify(client, times(1)).sendToServer(new SendMessageCommand("message test"));
+    }
+
+    @Ignore
+    @Test
+    public void shouldGetHistoryWhenPrintHistoryCommand() {
+        //When
+        client.message("/hist");
+
+        //Then
+        verify(client, times(1)).sendToServer(new ShowHistoryCommand());
     }
 }
