0958023b0b7e0d93047289eee9f77fbeb6142ca2
Yury Mysin <zlvlezhnin@gmail.com>
2017-09-07 12:59:11 +0300

    Looks like a working version

--- src/main/java/com/team3chat/client/ClientStartup.java
+++ src/main/java/com/team3chat/client/ClientStartup.java
@@ -4,8 +4,12 @@ import com.team3chat.client.parser.CommandParser;
 import com.team3chat.client.parser.DisconnectCommandMatcher;
 import com.team3chat.client.parser.SendMessageCommandMatcher;
 import com.team3chat.client.parser.ShowHistoryCommandMatcher;
-import com.team3chat.client.sender.CommandSender;
+import com.team3chat.client.sender.CommandSocketSender;
+import com.team3chat.client.ui.CommandPrinter;
+import com.team3chat.client.ui.CommandReader;
+import com.team3chat.client.ui.ConsolePrinterReader;
 import com.team3chat.client.ui.UserInputHandler;
+import com.team3chat.messages.Command;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -17,26 +21,44 @@ import static java.util.Arrays.asList;
  * Created by Java_9 on 07.09.2017.
  */
 public class ClientStartup {
+    private static CommandParser commandParser;
+    private static ConsolePrinterReader consolePrinterReader;
+
     public static void main(String[] args) {
-        CommandParser commandParser = new CommandParser(asList(
+        commandParser = new CommandParser(asList(
                 new SendMessageCommandMatcher(),
                 new ShowHistoryCommandMatcher(),
                 new DisconnectCommandMatcher()));
-        Thread userInputListeningThread = new Thread(() -> {
-            try (Socket socket = new Socket("127.0.0.1", 12345)) {
-                CommandSender commandsSender = new CommandSender(socket);
-                UserInputHandler inputHandler = new UserInputHandler(commandParser, commandsSender);
-                inputHandler.start();
-            } catch (UnknownHostException e) {
-                System.out.println("Client is pointed to non-existant server");
-            } catch (IOException e) {
-                System.out.println("Exception during connection to server occured");
-            }
-        });
-        userInputListeningThread.start();
-        Thread serverListeningThread = new Thread(() -> {
+        consolePrinterReader = new ConsolePrinterReader(commandParser);
+        try (Socket serverSocket = new Socket("127.0.0.1", 1234)) {
+            startUserListeningThread(serverSocket);
+            startSocketListeningThread(serverSocket, consolePrinterReader);
+        } catch (UnknownHostException e) {
+            System.out.println("Client is pointed to non-existant server");
+        } catch (IOException e) {
+            System.out.println("Exception during connection to server occured");
+        }
 
+    }
+
+    private static Thread startSocketListeningThread(Socket socket, CommandPrinter commandPrinter) {
+        Thread serverListeningThread = new Thread(() -> {
+            CommandReader reader = new CommandSocketSender(socket);
+            Command command = reader.readCommand();
+            commandPrinter.print(command);
         });
         serverListeningThread.start();
+        return serverListeningThread;
     }
+
+    private static Thread startUserListeningThread(Socket socket) {
+        Thread userInputListeningThread = new Thread(() -> {
+            UserInputHandler inputHandler = new UserInputHandler(consolePrinterReader, consolePrinterReader);
+            inputHandler.start();
+        });
+        userInputListeningThread.start();
+        return userInputListeningThread;
+    }
+
+
 }
