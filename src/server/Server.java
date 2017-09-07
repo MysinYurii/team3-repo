3f4f8a6e40d58359a74e4b4b15283f7f3d49ad14
teamDBSD <kaddm@yandex.ru>
2017-09-07 11:52:12 +0300

    server classes fixed (added methods to read and write)

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,14 +1,17 @@
 package server;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        HistoryWorker historyWorker = new HistoryWorker(new File("history.txt"));
         ServerSocket serverSocket = new ServerSocket(1234);
+
         while (true) {
-            new Thread(new Acceptor(serverSocket.accept())).start();
+            new Thread(new Acceptor(serverSocket.accept(), historyWorker)).start();
         }
     }
 }
