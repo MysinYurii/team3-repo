e1b5467b16af4180c30e56ec9e519eabb2e0a68d
Yury Mysin <zlvlezhnin@gmail.com>
2017-09-07 11:32:20 +0300

    Annotations

--- src/main/java/com/team3chat/client/parser/ShowHistoryCommandMatcher.java
+++ src/main/java/com/team3chat/client/parser/ShowHistoryCommandMatcher.java
@@ -1,15 +1,16 @@
 package com.team3chat.client.parser;
 
+import com.sun.istack.internal.Nullable;
 import com.team3chat.messages.ShowHistoryCommand;
 
 /**
  * Created by Java_9 on 07.09.2017.
  */
 public class ShowHistoryCommandMatcher implements CommandMatcher<ShowHistoryCommand> {
-    public static final String PREFIX = "/hist";
+    private static final String PREFIX = "/hist";
 
     @Override
-    public ShowHistoryCommand parseCommand(String commandPattern) {
+    public @Nullable ShowHistoryCommand parseCommand(String commandPattern) {
         return commandPattern.startsWith(PREFIX) ? new ShowHistoryCommand() : null;
     }
 }
