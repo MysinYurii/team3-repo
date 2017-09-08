9cbcb8c71e35246cc246dd8b304272236dcabc8e
Yury Mysin <zlvlezhnin@gmail.com>
2017-09-08 17:41:12 +0300

    Remove redundant variable

--- src/main/java/com/team3chat/client/Client.java
+++ src/main/java/com/team3chat/client/Client.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Objects;
 
@@ -17,8 +18,7 @@ public class Client {
     private Socket clientSocket;
 
     public Client() throws IOException {
-        String ip = "127.0.0.1";
-        clientSocket = new Socket(ip, 6667);
+        clientSocket = new Socket(InetAddress.getLocalHost(), 6667);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         out = new PrintWriter(clientSocket.getOutputStream(), true);
     }
