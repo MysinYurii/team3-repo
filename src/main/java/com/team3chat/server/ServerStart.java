9306b235b5f93703491ea86703d820429b6b06c0
teamDBSD <kaddm@yandex.ru>
2017-09-08 16:02:05 +0300

    last version
    - BUT no new file creating when last file is out of memory

--- src/main/java/com/team3chat/server/ServerStart.java
+++ src/main/java/com/team3chat/server/ServerStart.java
@@ -1,10 +1,16 @@
 package com.team3chat.server;
 
+import java.io.IOException;
+
 /**
  * Created by Java_12 on 08.09.2017.
  */
 public class ServerStart {
     public static void main(String[] args) {
-        new Server();
+        try {
+            new Server().start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
